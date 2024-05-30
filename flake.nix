f686afcb8e63019220dc7f85fa637ccde0d58355
Ruben Hönle <git@hoenle.xyz>
2024-05-30 15:38:59 +0200

    feat(nix): add support for nix run and nix containerimage build (#2)

--- flake.nix
+++ flake.nix
@@ -16,6 +16,30 @@
         inherit system;
       };
       treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+
+      python-script = pkgs.python311Packages.buildPythonApplication {
+        pname = "c2-pixler";
+        version = "0.1";
+        doCheck = false;
+        src = ./.;
+        propagatedBuildInputs = with pkgs; [
+          python311Packages.flask
+        ];
+        installPhase = ''
+          install -Dm755 ${./server.py} $out/bin/server.py
+          install -Dm755 ${./templates/index.html} $out/bin/templates/index.html
+        '';
+      };
+
+      containerImage = pkgs.dockerTools.buildLayeredImage {
+        name = "ghcr.io/golo300/c2-pixler";
+        tag = "unstable";
+        contents = with pkgs; [ cacert ];
+        config = {
+          Entrypoint = [ "${python-script}/bin/server.py" ];
+          Env = [ "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt" "PYTHONUNBUFFERED=1" ];
+        };
+      };
     in
     {
       formatter.${system} = treefmtEval.config.build.wrapper;
@@ -28,6 +52,13 @@
         buildInputs = [
           pkgs.python39Packages.flask
         ];
+      };
+
+      packages.${system} = {
+        default = pkgs.writeShellScriptBin "c2-pixler" ''
+          ${python-script}/bin/server.py "''${@:1}"
+        '';
+        containerImage = containerImage;
       };
     };
 }
